f96c1c2a6fe43235226baf7453a3086e7aa6000b
Hennadii Kupriiuk <hennadii.kupriiuk@gmail.com>
2024-07-12 22:13:13 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul>
+    <ResourceItem 
+      v-for="resource in storeResourses" 
+      :title="resource.title" 
+      :description="resource.description"
+      :link="resource.link">
+    </ResourceItem>
+  </ul>
+</template>
+
+<script>
+import ResourceItem from './components/ResourceItem.vue'
+
+export default {
+  data() {
+    return {
+      storeResourses: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com'
+        },
+      ]
+    }
+  },
+  components: {
+    ResourceItem,
+  }
+}</script>
---
+++ src/components/ResourceItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <li>
+    <h1>{{ title }}</h1>
+    <a :href="link">
+      <p>{{ description }}</p>
+    </a>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['title', 'description', 'link']
+}
+</script>
